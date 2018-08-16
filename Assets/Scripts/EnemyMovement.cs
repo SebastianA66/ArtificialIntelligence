058e004971edbeefc6d52b7f8387b4cf2ae65043
SebastianA66 <42424010+SebastianA66@users.noreply.github.com>
2018-08-17 09:18:23 +1000

    Initial commit

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour
+{
+    
+    public Transform[] waypoints;
+    private int destPoint = 0;
+    private NavMeshAgent agent;
+
+     
+    // Use this for initialization
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.autoBraking = false;
+
+        GoToNextPoint();
+    }
+
+    void GoToNextPoint()
+    {
+        if (waypoints.Length == 2)
+            return;
+
+        agent.destination = waypoints[destPoint].position;
+        destPoint = (destPoint + 1) % waypoints.Length;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!agent.pathPending && agent.remainingDistance < 2f)
+            GoToNextPoint();
+    }
+}
