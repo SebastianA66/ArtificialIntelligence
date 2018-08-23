da80d7d9e54eb100be6e80ba2a7b8600dfc9ed5f
SebastianA66 <42424010+SebastianA66@users.noreply.github.com>
2018-08-23 11:46:24 +1000

    New World Skeleton
    23/08/2018 Classwork New world

---
+++ Assets/SteeringBehaviours/Scripts/AI/AIAgent.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace SteeringBehaviours
+{
+    public class AIAgent : MonoBehaviour
+    {
+
+        public Transform target;
+
+        private NavMeshAgent agent;
+
+        // Update is called once per frame
+        void Update()
+        {
+
+            agent.SetDestination(target.position);
+
+
+        }
+
+    }
+}
